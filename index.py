bff24550ada684ab7b0f138c88beed09eef46375
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-03 20:44:18 -0500

    feat: add UI with tkinter

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import openai
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -5,6 +6,8 @@ _ = load_dotenv(find_dotenv())
 from utils.helpers import define_model
 
 openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+root = Tk()
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 chunk_size = 1000 # tamaño de cada fragmento
@@ -46,13 +49,32 @@ qa_chain = RetrievalQA.from_chain_type(
     retriever=vectordb.as_retriever(K = 5)
 )
 
-while True:
-    pregunta = input("Hola Mundo! Estoy listo para tu pregunta :)    ")
-    if pregunta == "FIN":
-        break 
-    else:
-        result = qa_chain({"query": pregunta})
-        print('---')    
-        print(result["result"])
-        print('---')    
-        print()
+# while True:
+#     pregunta = input("Hola Mundo! Estoy listo para tu pregunta :)    ")
+#     if pregunta == "FIN":
+#         break 
+#     else:
+#         result = qa_chain({"query": pregunta})
+#         print('---')    
+#         print(result["result"])
+#         print('---')    
+#         print()
+
+root.title('Python Windows Chat with GPT')
+root.geometry("500x300")
+root.resizable(0, 0)
+
+# pregunta = "Es necesario saber matemática para ser un experto en IA?"
+pregunta = StringVar()
+def askGPT():
+    label2 = Label(root, text=qa_chain({"query": pregunta.get()})["result"])
+    label2.grid(row=2, column=0)
+
+label1 = Label(root, text="Tu pregunta aquí: ")
+label1.grid(row=0, column=0)
+entrada1 = Entry(root, textvariable=pregunta)
+entrada1.grid(row=0, column=1)
+ask_btn = Button(root, text="Preguntar", command=askGPT)
+ask_btn.grid(row=1, column=0)
+
+root.mainloop()
